00763b600d4b2f37a9593fd81db5e1806bdf9604
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2025-09-17 21:31:54 -0300

    Cosovichs

---
+++ js/clipboard.js
@@ -0,0 +1,10 @@
+﻿window.clipboardCopy = {
+    copyText: function (text) {
+        navigator.clipboard.writeText(text).then(function () {
+            // Éxito al copiar
+        })
+            .catch(function (error) {
+                console.error('Error al copiar el texto: ', error);
+            });
+    }
+};
