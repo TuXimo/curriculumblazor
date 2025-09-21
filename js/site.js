212324cc7494291a6892f0b9056ab569d2d995ae
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2025-09-21 00:54:42 -0300

    a

---
+++ js/site.js
@@ -0,0 +1,7 @@
+﻿window.toggleDarkMode = function (isDark) {
+    if (isDark) {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+};
