c8824f158504742c1a6997a457eaa7758cc53433
Máximo Brunetti <67173706+TuXimo@users.noreply.github.com>
2025-10-22 08:53:29 -0300

    GODETO

--- js/site.js
+++ js/site.js
@@ -1,30 +1,23 @@
 ﻿window.toggleDarkMode = function (isDark) {
+    const root = document.documentElement; // <html>
     if (isDark) {
-        document.body.classList.add("dark-mode");
+        root.classList.add("dark-mode");
         localStorage.setItem("theme", "dark");
     } else {
-        document.body.classList.remove("dark-mode");
+        root.classList.remove("dark-mode");
         localStorage.setItem("theme", "light");
     }
 };
 
+
 // Al cargar, aplicar el tema guardado
 window.applySavedTheme = function () {
-    let theme = localStorage.getItem("theme");
-    let isDark;
-
-    if (theme) {
-        isDark = theme === "dark";
-    } else {
-        // Si no hay tema guardado, usar la preferencia del sistema
-        isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const root = document.documentElement;
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+        root.classList.add("dark-mode");
+        return true;
     }
-
-    if (isDark) {
-        document.body.classList.add("dark-mode");
-    } else {
-        document.body.classList.remove("dark-mode");
-    }
-
-    return isDark;
+    return false;
 };
+
